58d67884ca95d44c49b5e9a278ada6c1a4f91d08
Sthefani Laura <slauraic@hotmail.com>
2018-10-23 21:09:22 -0300

    Criar instâncias e como usar as props nelas

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
 const divisaoProjeto = document.getElementById("projeto");
 
+function Link(props){
+    return <a className="link" href={props.url}>{props.children}</a>
+}
+function Botao(props){
+    return <input className={props.className} type="submit" value={props.children}/>
+}
 
-const linkCriarUmaConta = <a class="link" href="/conta">Criar uma conta</a>
 
-const linkFazerLogin = <a class="link" href="/login">Fazer login</a>
-
-const formulario = (
-    <form>
-        {[linkCriarUmaConta, linkFazerLogin]}
-    </form>
+const paginaLogin = (
+    <main className="pagina-login">
+        <h1>Login</h1>
+        <p>Entre com seu e-mail e senha.</p>
+        <Botao className="botao">Enviar</Botao>
+        <Link url="/conta">Criar uma conta</Link>   
+    </main>
 )
 
-
-ReactDOM.render(formulario, divisaoProjeto);
+ReactDOM.render(paginaLogin, divisaoProjeto);
