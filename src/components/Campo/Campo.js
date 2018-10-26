f96858eb556fcb125518457c16acbf6dbf13d91f
Sthefani Laura <slauraic@hotmail.com>
2018-10-25 22:31:42 -0300

    Alterar função para class

--- src/components/Campo/Campo.js
+++ src/components/Campo/Campo.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import './Campo.css';
 
-function Campo (props) {
+class Campo extends React.Component {
+    
+    constructor (props){
+        super(props);
+        this.state ={
+            erro:'',
+        }
+    }
+    validar = (event) => {
+        const alvo = event.target;
+        if(this.props.obrigatorio && alvo.value.trim() === ''){
+            const state = {
+                erro:'Campo obrigatório'
+            }
+        
+        this.setState(state);
+        }
+    }
+    render(){
+        
+        return (
+                <div>
+                    <input 
+                        className="caixa-texto" 
+                        id={this.props.id} 
+                        type={this.props.type} 
+                        name={this.props.name} 
+                        placeholder={this.props.placeholder}
+                        onChange={this.validar}
+                    />
+                    <p className="grupo__erro">
+                        {this.state.erro}
+                    </p>
 
-    return <input className="caixa-texto" id={props.id} type={props.type} name={props.name} placeholder={props.placeholder}/>
+                    
+                </div>
+                
+                
+                )
+    }
+    
     
 }
 
